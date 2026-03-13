a6d34f0246ef9433d485f93b7dd8d23157e4b26a
cnormand <cyril.normand@etu.utc.fr>
2026-03-13 20:58:14 +0100

    SQL

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ class Config:
 
     SECRET_KEY = "supersecret"
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db") 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = "jwt-secret"
